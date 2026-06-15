b1fc7d9f3fb87011817694e059c0be0f56c89175
Ismael Alvarado <ismael.alvaradoperez@ucr.ac.cr>
2026-06-15 04:20:09 +0000

    Despliegue exitoso de pantalla, se necesita esta linea en compilacion -lraylib -lm -lpthread -ldl -lrt -lX11

--- interfaz.c
+++ interfaz.c
@@ -1,28 +1,22 @@
 #include "raylib.h"
 
------------------------------------------------------------------------------------
 int main(void)
 {
     // Inicialización
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
     // Main interfaz loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
-        //----------------------------------------------------------------------------------
         // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
 
         // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
@@ -30,13 +24,10 @@ int main(void)
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
-    //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
 
     return 0;
 }
